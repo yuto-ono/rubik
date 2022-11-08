105bd90cd1a01390c22b806a45e598ed1abe9c65
Yuto Ono <stellarwindmaster@gmail.com>
2022-11-08 14:29:26 +0900

    Update constants.ts

--- src/rubik/constants.ts
+++ src/rubik/constants.ts
@@ -1,5 +1,5 @@
-export const CUBE_SIZE = 6000
-export const SCREEN_DISTANCE = 10000
+export const CUBE_SIZE = 10000
+export const SCREEN_DISTANCE = 20000
 export const UNIT_VECTOR_BITS = 8
 export const DRAG_LIMIT = 900
 export const ROTATION_SPEED = 0.005
