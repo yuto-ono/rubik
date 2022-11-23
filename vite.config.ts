c2b423939fe91b324eff98dba0f869ac82941b7e
Yuto Ono <stellarwindmaster@gmail.com>
2022-11-23 18:02:30 +0900

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: "ルービックキューブ by Yuto",
+        short_name: "ルービックキューブ",
+        description:
+          "Web上でルービックキューブを遊べるサイトです。キューブの外側を動かすとキューブ全体が動きます。キューブ本体をスワイプすると回転します。",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "./",
+      },
+    }),
+  ],
 })
